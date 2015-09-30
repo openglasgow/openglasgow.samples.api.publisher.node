5f4d9abbd47fa133613cc6ae8af310e330eef469
Steven Livingstone-Perez <steven@livz.org>
2015-09-30 15:53:38 +0100

    Added organisation, dataset and resource querying.

---
+++ bin/config.js
@@ -0,0 +1,11 @@
+var config = {};
+
+// properties
+config.Authbase = 'https://login.windows.net/';
+config.TenantId = 'open.glasgow.gov.uk';
+config.ResourceId = 'http://GCCCTPECServicesPrep.cloudapp.net:8080/';
+config.ClientId = ''; //ckan
+config.ClientKey = '';
+config.SubscriptionKey = ''; //startup
+
+module.exports = config;
